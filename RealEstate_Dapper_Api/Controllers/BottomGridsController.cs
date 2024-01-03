b7a1abebda6cbc8edbee5e9d5853c7867298fe17
mmtkocak <kocakmehmet4242@gmail.com>
2024-01-03 17:11:56 +0300

    Ders 49 Sonu

--- RealEstate_Dapper_Api/Controllers/BottomGridsController.cs
+++ RealEstate_Dapper_Api/Controllers/BottomGridsController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
-using RealEstate_Dapper_Api.Dtos.ServiceDtos;
 using RealEstate_Dapper_Api.Repositories.BottomGridRepository;
 
 namespace RealEstate_Dapper_Api.Controllers
