638a4acc383f4f22cc48c4be7df433aefc96d5a3
mmtkocak <mehmet-kocak@outlook.com.tr>
2024-05-07 16:53:58 +0300

    Ders 66 Sonu - Dashboard Chart Api

---
+++ RealEstate_Dapper_Api/Repositories/EstateAgentRepositories/DashboardRepositories/LastProductRepositories/Last5ProductsRepository.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.LastProductRepositories
+{
+    public class Last5ProductsRepository : ILast5ProductsRepository
+    {
+        private readonly Context _context;
+
+        public Last5ProductsRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultLast5ProductWithCategoryDto>> GetLast5ProductAsync(int id)
+        {
+            string query = "Select Top(5) ProductID,Title,Price,City,District,ProductCategory,CategoryName,AdversitementDate from" +
+                " Product Inner Join Category on Product.ProductCategory=Category.CategoryID where EmployeeID=@employeeID order by ProductID desc";
+            var parameters = new DynamicParameters();
+            parameters.Add("@employeeID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultLast5ProductWithCategoryDto>(query,parameters);
+                return values.ToList();
+            }
+        }
+    }
+}
